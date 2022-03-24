984a2eaf4d078b806fdc1ea3dc417c8eed3b4522
Ostap Seryvko <ostap.seryvko@gmail.com>
2022-03-24 15:35:23 +0200

    task 4 done

--- game.py
+++ game.py
@@ -1,17 +1,17 @@
+kills=0
 class Room:
     def __init__(self, name, north=None, south=None, east=None, west=None, character=None, item=None) -> None:
-        self.name=name
-        self.description=''
-        self.north=north
-        self.south=south
-        self.east=east
-        self.west=west
-        self.character=character
-        self.item=item
-        
+        self.name = name
+        self.description = ''
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
+        self.character = character
+        self.item = item
 
     def set_description(self, description):
-        self.description=description
+        self.description = description
 
     def link_room(self, other, direction):
         if direction == 'north':
@@ -24,17 +24,23 @@ class Room:
             self.east = other
 
     def set_character(self, chara):
-        self.character=chara
+        self.character = chara
 
     def set_item(self, item):
-        self.item=item
-    
-    def get_details(self):
-        return f'room {self.name}' + '\n' + f'{self.description}' + '\n'
+        self.item = item
 
+    def get_details(self):
+        print(f'{self.name}' + '\n' + '----------------' +'\n'+ f'{self.description}')
+        if self.north:
+            print(f'The {self.north.name} is north')
+        if self.south:
+            print(f'The {self.south.name} is south')
+        if self.west:
+            print(f'The {self.west.name} is west')
+        if self.east:
+            print(f'The {self.east.name} is east')
     def get_character(self):
-        char=Character(self.name)
-        return char 
+        return self.character
 
     def get_item(self):
         return self.item
@@ -48,43 +54,72 @@ class Room:
             return self.west
         elif command == 'east':
             return self.east
-        
-
-class Character(Room):
-    def __init__(self, name, north=None, south=None, east=None, west=None, character=None, item=None) -> None:
-        super().__init__(name, north, south, east, west, character, item)
-    
-    def talk(self):
-        return self.north
 
 
-class Enemy:
+class Character:
     def __init__(self, name, desc) -> None:
-        self.name=name
-        self.desc=desc
-        self.conversation=''
-        self.weakness=''
+        self.name = name
+        self.desc = desc
+        self.conversation = ''
 
     def set_conversation(self, phrase):
-        self.conversation=phrase
+        self.conversation = phrase
     
+    def describe(self):
+        print(f'{self.name} is here')
+        print(self.desc)
+
+    def talk(self):
+        print(f'[{self.name}] says: {self.conversation}')
+
+
+class Friend(Character):
+    def __init__(self, name, desc) -> None:
+        super().__init__(name, desc)
+
+
+
+
+class Enemy(Character):
+    def __init__(self, name, desc) -> None:
+        super().__init__(name, desc)
+        self.weakness = '' 
+        
+
     def set_weakness(self, item):
-        self.weakness=item
+        self.weakness = item
+
+    def fight(self, item):
+        if item == self.weakness:
+            global kills
+            kills += 1
+            return True
+        return False
+
+    def get_defeated(self):
+        global kills
+        return kills
+
 
 class Item:
     def __init__(self, name) -> None:
-        self.name=name
-        self.description=''
+        self.name = name
+        self.description = ''
 
     def set_description(self, desc):
-        self.description=desc
-    
+        self.description = desc
+
     def describe(self):
-        return self.description
+        print(f'the [{self.name}] is here - {self.description}')
 
-room1=Room('kitchen')
-room2=Room('hallway')
+    def get_name(self):
+        return self.name
 
-room1.link_room(room2, 'north')
-char=room1.get_character()
-print(char.talk())
+
+# room1 = Room('kitchen')
+# room2 = Room('hallway')
+
+# room1.link_room(room2, 'north')
+# char = Enemy('dudee', 'meme')
+# print(char.set_conversation('lol'))
+# print(char.conversation)
